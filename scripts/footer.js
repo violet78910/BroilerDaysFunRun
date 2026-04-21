277a54a29019e0e5329f898c18d01a0e74dacf61
Peyton Nelson <79875125+cpeyton78910@users.noreply.github.com>
2026-04-21 00:39:20 -0500

    Update footer.js
    Updated footer styles and added responsive design for smaller screens.

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,22 +1,38 @@
-const head = document.querySelector('head');
-head.innerHTML += `
+const body = document.querySelector('body');
+const currentYear = new Date().getFullYear();
+body.innerHTML += `
   <style>
     footer {
-    font-size: 8.0pt;
-    text-align: center;
-    width: 100%;
-    margin-top: 2em;
+      font-size: 8.0pt;
+      text-align: center;
+      width: 100%;
+      margin-top: 2rem;
+    }
+
+    footer span {
+      margin: 0 10vw;
+      padding: 2rem 0;
+    }
+
+    @media only screen and (max-width: 768px) {
+
+    footer span {
+      margin: 0 1rem;
+    }
+
   }
 
-  footer span {
-    margin: 0 10vw;
-    padding: 2em 0;
-  }
-`;
+  @media only screen and (max-width: 480px) {
 
-const currentYear = new Date().getFullYear();
-const footer = document.querySelector('footer');
-footer.innerHTML = `
-  <span>&copy; ${currentYear} Arcadia Lions Club</span>
-  <span>&copy; ${currentYear} Broiler Days Fun Run</span>
+    footer span {
+      display: block;
+    }
+
+  }
+    
+  </style>
+  <footer>
+    <span>&copy; ${currentYear} Arcadia Lions Club</span>
+    <span>&copy; ${currentYear} Broiler Days Fun Run</span>
+  </footer>
 `;
